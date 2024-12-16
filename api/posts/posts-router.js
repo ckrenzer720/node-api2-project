4851f55102b6cd980bf35d89904ab863ec8def13
Conner Krenzer <krenzer84@gmail.com>
2024-12-16 12:20:07 -0500

    fin

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -61,12 +61,43 @@ router.post("/", (req, res) => {
 });
 
 // update()
-router.put("/:id", (req, res) => {});
+router.put("/:id", (req, res) => {
+  const { title, contents } = req.body;
+  if (!title || !contents) {
+    res
+      .status(400)
+      .json({ message: "Please provide title and contents for the post" });
+  } else {
+    Post.findById(req.params.id)
+      .then((post) => {
+        if (!post) {
+          res
+            .status(404)
+            .json({ message: "The post with the specified ID does not exist" });
+        } else {
+          return Post.update(req.params.id, req.body);
+        }
+      })
+      .then((data) => {
+        if (data) {
+          return Post.findById(req.params.id);
+        }
+      })
+      .then((post) => {
+        if (post) {
+          res.status(200).json(post);
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+        res
+          .status(500)
+          .json({ message: "The post information could not be modified" });
+      });
+  }
+});
 
 // remove()
-// .catch(error = {
-//     res.status(500).json({ message: "The post could not be removed" })
-// })
 router.delete("/:id", async (req, res) => {
   try {
     const postId = await Post.findById(req.params.id);
@@ -84,6 +115,23 @@ router.delete("/:id", async (req, res) => {
 });
 
 // findPostComments()
-router.get("/:id/comments", (req, res) => {});
+router.get("/:id/comments", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post) {
+      res
+        .status(404)
+        .json({ message: "The post with the specified ID does not exist" });
+    } else {
+      const data = await Post.findPostComments(req.params.id);
+      res.json(data);
+    }
+  } catch (error) {
+    console.log(error);
+    res
+      .status(500)
+      .json({ message: "The comments information could not be retrieved" });
+  }
+});
 
 module.exports = router;
