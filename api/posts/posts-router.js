ef904d1a2e875b96b55ece626fd245f71d562add
Conner Krenzer <krenzer84@gmail.com>
2024-12-16 10:52:41 -0500

    "router.GET set up"

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,20 @@
 // implement your posts router here
+const express = require("express");
+const Posts = require("./posts-model");
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  Posts.find(req.query)
+    .then((posts) => {
+      res.status(200).json(posts);
+    })
+    .catch((error) => {
+      console.log(error);
+      res
+        .status(500)
+        .json({ message: "The posts information could not be retrieved" });
+    });
+});
+
+module.exports = router;
